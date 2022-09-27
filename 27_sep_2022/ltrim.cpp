014490a3cb88b83b443697dc4d2f43d2f79cac4f
Arpi <111233352+arpimusoyan@users.noreply.github.com>
2022-09-27 20:55:53 +0400

    Add files via upload

---
+++ 27_sep_2022/ltrim.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+std::string ltrim(std::string str)
+{
+	int count = 0;
+  for(int i = 0; i < str.size(); i++)
+  {
+   if(str[i] == ' ')
+   {
+     count++;
+    }
+   else break;
+  }
+  str.erase(0,count);
+
+return str;
+
+}
+int main()
+{
+ std::string str = "    hello" ;
+    std::cout << ltrim(str) << std::endl;
+    return 0;
+}
