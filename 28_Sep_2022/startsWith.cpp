b503d450b380d9417e30c3705dd22c4b7dc3481c
Arpi <111233352+arpimusoyan@users.noreply.github.com>
2022-09-28 15:21:10 +0400

    Add files via upload

---
+++ 28_Sep_2022/startsWith.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+bool startsWith(std::string str, std::string t)
+{
+  int count = 0;
+  int size = t.size();
+  for(int i = 0; i < t.size(); ++i)
+  {
+      if (str[i] == t[i])
+      {
+        count++;
+      }
+      if (count == t.size())
+      {
+        return true;
+      }
+  }
+    return false;
+  
+}
+int main()
+{
+  std::string str = "hello world";
+  std::string t = "hell";
+  std::cout << startsWith(str, t);
+}
